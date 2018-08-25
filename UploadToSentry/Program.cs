eb40ae53a499dc4b94ef3da3b77485c8e85be680
Alexander Kyte <alkyte@microsoft.com>
2018-08-25 00:58:56 -0400

    Dump progress on json based uploader, made github repo

--- UploadToSentry/Program.cs
+++ UploadToSentry/Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using Newtonsoft.Json.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace UploadToSentry
 {
@@ -27,7 +28,7 @@ namespace UploadToSentry
             var increment = groups[2].Value;
 
             var version_string = Regex.Match(payload["configuration"]["version"].ToString(), @"").Groups;
-
+            var test_name = "test";
 
             var ravenClient = new RavenClient(url);
             ravenClient.BeforeSend = requester =>
@@ -44,7 +45,7 @@ namespace UploadToSentry
             }
             catch (Exception exc) {
                 var blob = new SentryEvent(exc);
-                // blob.Contexts.Runtime.RawDescription = );
+                //blob.Contexts.Runtime.Version
                 blob.Contexts.Device.Architecture = payload["configuration"]["architecture"].ToString();
                 blob.Contexts.OperatingSystem.Name = os_tag;
                 // blob.Contexts.App.Identifier = test_name;
@@ -54,7 +55,89 @@ namespace UploadToSentry
                 blob.Fingerprint.Add(increment);
                 ravenClient.Capture (blob);
             }
-        }
+
+            var event_id = new JProperty("event_id", "f432134b85314b7bb5ad7af568ee278a");
+            //var culprit = new JProperty("culprit", "");
+            var timestamp = new JProperty("timestamp", "\t\"timestamp\": \"2018-08-24T14:52:53.382084Z\",\n");
+            var exc_objs = new List<JObject> ();
+            var failure_type = "Unhandled Managed Exception";
+
+            var stackTraces = payload["threads"] as JArray;
+            if (stackTraces.Count > 1)
+                failure_type = "Unhandled Unmanaged Exception";
+
+            for (int i=0; i < stackTraces.Count; i++){
+                exc_objs.Add(new JObject(
+                   new JProperty("module", test_name),
+                   new JProperty("type", failure_type),
+                   new JProperty("value", ""),
+                   new JProperty("culprit", "")));
+            }
+
+           
+            var exception = new JProperty("exception", new JArray(exc_objs.ToArray));
+
+
+
+            //var accum = new JObject(event_id, culprit, timestamp, exception);
+
+
+            "exception": [{
+                "module": "UploadToSentry",
+                "stacktrace": {
+                    "frames": [{
+                        "abs_path": null,
+                        "colno": 0,
+                        "filename": "/Users/akyte/Projects/UploadToSentry/UploadToSentry/Program.cs",
+                        "function": "Upload",
+                        "in_app": true,
+                        "lineno": 43,
+                        "module": "UploadToSentry.Uploader",
+                        "post_context": null,
+                        "pre_context": null,
+                        "context_line": "Void Upload(System.String, System.String, System.String)",
+                        "vars": null
+                    }]
+                },
+                "type": "Exception",
+                "value": "boop"
+            }],
+
+            //        new JProperty("channel",
+            //          new JObject(
+            //              new JProperty("title", "James Newton-King"),
+            //              new JProperty("link", "http://james.newtonking.com"),
+            //              new JProperty("description", "James Newton-King's blog."),
+            //              new JProperty("item",
+            //                  new JArray(
+            //                      from p in posts
+            //                      orderby p.Title
+            //                      select new JObject(
+            //                          new JProperty("title", p.Title),
+            //                          new JProperty("description", p.Description),
+            //                          new JProperty("link", p.Link),
+            //                          new JProperty("category",
+            //                              new JArray(
+            //                                  from c in p.Categories
+            //                                  select new JValue(c)))))))));
+
+            //Console.WriteLine(rss.ToString());
+
+
+            //{
+                //"event_id": "fc6d8c0c43fc4630ad850ee518f1b9d0",
+                //"culprit": "my.module.function_name",
+                //"timestamp": "2011-05-02T17:41:36",
+                //"tags": {
+                //    "ios_version": "4.0"
+                //},
+                //"exception": [{
+                //    "type": "SyntaxError",
+                //    "value": "Wattttt!",
+                //    "module": "__builtins__"
+                //}]
+            }
+
 
         public static void Main(string[] args)
         {
